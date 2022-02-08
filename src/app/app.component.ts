2e78a79bd30c9497010d5c38a7470f424ca2e717
Renegade Asturias <renegade.asturias@gmail.com>
2022-02-08 01:55:41 +0100

    Proyecto Empleados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { Empleado } from './empleado.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'empleados';
+  titulo = 'Listado de Empleados';
+
+  empleados:Empleado[] = [
+    new Empleado("Juan","Díaz","Presidente",7500),
+    new Empleado("Ana","Martín","Directora",7500),
+    new Empleado("María","Fernandez","Jefa sección",7500),
+    new Empleado("Laura","López","Administrativo",7500),
+  ];
+
+  agregarEmpleado(){
+    let miEmpleado=new Empleado(this.cuadroNombre, this.cuadroApellido, this.cuadroCargo, this.cuadroSalario);
+    this.empleados.push(miEmpleado);
+  }
+
+  cuadroNombre:string="";
+  cuadroApellido:string="";
+  cuadroCargo:string="";
+  cuadroSalario:number=0;
+
 }
