31e921897ce20121cd0c9124cada00ee6c515d8f
Renegade Asturias <renegade.asturias@gmail.com>
2022-02-15 17:01:19 +0100

    Proyecto Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,15 +7,33 @@ import { EmpleadoHijoComponent } from './empleado-hijo/empleado-hijo.component';
 import { EmpleadoHijoCaracteristicasComponent } from './empleado-hijo-caracteristicas/empleado-hijo-caracteristicas.component';
 import { ServicioEmpleadosService } from './servicio-empleados.service';
 import { EmpleadosService } from './empleados.service';
+import { HomeComponentComponent } from './home-component/home-component.component';
+import { ProyectosComponentComponent } from './proyectos-component/proyectos-component.component';
+import { QuienesComponentComponent } from './quienes-component/quienes-component.component';
+import { ContactoComponentComponent } from './contacto-component/contacto-component.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes:Routes=[
+	{path:'', component:HomeComponentComponent},
+	{path:'proyectos', component:ProyectosComponentComponent},
+	{path:'quienes', component:QuienesComponentComponent},
+	{path:'contacto', component:ContactoComponentComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     EmpleadoHijoComponent,
-    EmpleadoHijoCaracteristicasComponent
+    EmpleadoHijoCaracteristicasComponent,
+    HomeComponentComponent,
+    ProyectosComponentComponent,
+    QuienesComponentComponent,
+    ContactoComponentComponent
   ],
   imports: [
-    BrowserModule, FormsModule
+    BrowserModule, 
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [
     ServicioEmpleadosService,
