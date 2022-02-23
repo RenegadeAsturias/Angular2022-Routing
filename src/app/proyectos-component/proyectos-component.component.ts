af6c9108a802bab3da3000b172bf2ed336b41c5c
Renegade Asturias <renegade.asturias@gmail.com>
2022-02-24 00:52:03 +0100

    Proyecto routing, ejemplos redireccionamiento

--- src/app/proyectos-component/proyectos-component.component.ts
+++ src/app/proyectos-component/proyectos-component.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Empleado } from '../empleado.model';
+import { EmpleadosService } from '../empleados.service';
 
 @Component({
   selector: 'app-proyectos-component',
@@ -7,9 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProyectosComponentComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router, private miServicioEmpleados:EmpleadosService) { }
 
   ngOnInit(): void {
+    this.empleados = this.miServicioEmpleados.empleados;    
+  }
+
+  empleados:Empleado[]=[];
+
+  agregarEmpleado(){
+    let miEmpleado=new Empleado(this.cuadroNombre, this.cuadroApellido, this.cuadroCargo, this.cuadroSalario);
+    this.miServicioEmpleados.agregarEmpleadoServicio(miEmpleado);
+    // this.miServicio.muestraMensaje("Empleado añadido:"+miEmpleado.nombre);
+    
+    this.router.navigate(['']); // Redireccionamiento automático para ir al Home después de agregar un Empleado
+  }
+
+  cuadroNombre:string="";
+  cuadroApellido:string="";
+  cuadroCargo:string="";
+  cuadroSalario:number=0;
+
+  irHome(){
+    this.router.navigate(['']);
+  }
+  irContacto(){
+    this.router.navigate(['contacto']);
   }
 
 }
